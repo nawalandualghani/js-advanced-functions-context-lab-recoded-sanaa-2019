6c84d3c830d496a998e7abb045ead154e5b7ae30
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-27 07:50:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,6 +39,9 @@ function createEmployeeRecord(array){
 
        return this
  }
+ function hoursWorkedOnDate(){
+   
+ }
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
